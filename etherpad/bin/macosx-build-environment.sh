eddaf86233758843ece40e1c1c72ea5136444e9c
Marcello Barnaba <vjt@openssl.it>
2009-12-24 14:35:50 +0100

    Added build environment for Mac OS X

---
+++ etherpad/bin/macosx-build-environment.sh
@@ -0,0 +1,15 @@
+# For etherpad OSS
+# Mac OS X: 10.6.2
+# MacPorts:  1.8.0
+#
+# - vjt@openssl.it
+#
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home"
+export SCALA_HOME="/opt/local/share/scala"
+export JAVA="$JAVA_HOME/bin/java"
+export SCALA="$SCALA_HOME/bin/scala"
+export PATH="$JAVA_HOME/bin:$SCALA_HOME/bin:/opt/local/lib/mysql5/bin:$PATH"
+export MYSQL_CONNECTOR_JAR="/Library/Java/Extensions/mysql-connector-java-5.1.10-bin.jar"
+export POSTGRESQL_CONNECTOR_JAR="/Library/Java/Extensions/mysql-connector-java-5.1.10-bin.jar"
+export DB_ADAPTER="mysql"
+# -
